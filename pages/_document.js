38cda01a4de1f2010a7dac411ea61ab7f93e98f6
waldemirflj <waldemirflj@gmail.com>
2021-12-15 10:17:00 -0300

    configura onesignal

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from 'styled-components'
-
+import { ServerStyleSheet } from "styled-components";
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,9 +21,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
   render() {
@@ -36,6 +35,11 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/favicon.png" />
           <link rel="apple-touch-icon" type="image/png" href="/favicon.png" />
           <meta name="theme-color" content="#fff" />
+
+          <script
+            src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+            async=""
+          ></script>
         </Head>
         <body>
           <Main />
